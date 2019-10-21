0d4f1e6dec54394907c5536bcd28014689acb03a
Javier <javierramon@outlook.com>
2019-10-21 19:46:22 +0200

    Refactorizacion del codigo del ejercicio de ejemplo de las estrellas del tema 13.

--- PARTE II/PROYECTO 1/12.- Ship That Fires Bullets/Ejercicios Independientes/game_functions.py
+++ PARTE II/PROYECTO 1/12.- Ship That Fires Bullets/Ejercicios Independientes/game_functions.py
@@ -1,3 +1,5 @@
+from star import Star
+
 def get_number_stars(screen, star):
     available_space = 500 - 2 * star.rect.width
     number_stars = int(available_space / (2 * star.rect.width))
@@ -7,3 +9,20 @@ def get_number_rows(screen, star):
     available_space = 500 - 2 * star.rect.height
     number_rows = int(available_space / (2 * star.rect.width))
     return number_rows
+
+def create_star(screen, stars, star_number, row_number):
+    star = Star(screen)
+    star.rect.x = star.rect.width + 2 * star.rect.width * star_number
+    star.rect.y = star.rect.width + 2 * star.rect.height * row_number
+    stars.add(star)
+
+def create_constellation(screen, stars):
+    star = Star(screen)
+    star.width = star.rect.width
+
+    number_stars = get_number_stars(screen, star)
+    number_rows = get_number_rows(screen, star)
+
+    for row_number in range(number_rows):
+        for star_number in range(number_stars):
+            create_star(screen, stars, star_number, row_number)
--- PARTE II/PROYECTO 1/12.- Ship That Fires Bullets/Ejercicios Independientes/starrysky.py
+++ PARTE II/PROYECTO 1/12.- Ship That Fires Bullets/Ejercicios Independientes/starrysky.py
@@ -1,45 +1,28 @@
-import pygame
-import sys
-from star import Star
-from pygame.sprite import Group
-import game_functions as gf
+# Se IMPORTAN LAS LIBRERIAS NECESARIAS PARA LA APP.
+import pygame   #
+import sys      #
+from pygame.sprite import Group #
+import game_functions as gf     #
 
-
-
-def create_star(screen, stars, star_number, row_number):
-    star = Star(screen)
-    star.rect.x = star.rect.width + 2 * star.rect.width * star_number
-    star.rect.y = star.rect.width + 2 * star.rect.height * row_number
-    stars.add(star)
-
-def create_constellation(screen, stars):
-    star = Star(screen)
-    star.width = star.rect.width
-
-    number_stars = gf.get_number_stars(screen, star)
-    number_rows = gf.get_number_rows(screen, star)
-
-    for row_number in range(number_rows):
-        for star_number in range(number_stars):
-            create_star(screen, stars, star_number, row_number)
 
 def run_game():
+
     pygame.init()
     screen = pygame.display.set_mode((500, 500))
     pygame.display.set_caption('Starry Sky')
 
     stars = Group()
-    create_constellation(screen, stars)
+    
+    gf.create_constellation(screen, stars)
 
     while True:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
-            
+                sys.exit()  
         screen.fill((255,255,250))
         stars.draw(screen)
         pygame.display.flip()
+
 
 run_game()
     
